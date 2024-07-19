8374fbfb8e0938b92d0d89fd21e8363d89ce89a4
Fernando Silva <106024845+Fnord484@users.noreply.github.com>
2024-07-19 11:38:24 -0300

    Update Home.py
    Adding switch page buttons to all pages in the top area and the switch page to Help page in sidebar. Another thing is change the styles of some texts

--- Home.py
+++ Home.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from PIL import Image
+from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(
     page_title='Home',
     page_icon='🎲',
-    layout='centered'
+    layout='wide',
+    initial_sidebar_state='collapsed'
 )
-image_path = 'target.png'
+image_path = 'C:/Users/Fernando/OneDrive/TRAB/repos/target.png'
 image = Image.open(image_path)
 st.image(image, width=50)
 
@@ -14,31 +16,43 @@ st.write( '# Fome Zero Company')
 
 st.markdown(
     '''
-    Este Dashboard foi construído para o acompanhamento das métricas e crescimento dos Países, Cidades e Restaurantes cadastrados.
-    ##### Como Utilizar este Growth Dashboard?
-    - Obs: Utiilize os filtros da barra lateral para auxílio no estudo e visualização.
+    ##### Este conjunto de dashboard foi construído para o acompanhamento das métricas e crescimento dos *Países, Cidades e Restaurantes cadastrados.*
+    ##### :orange[Selecione o Dashboard nos botões abaixo ou na barra lateral]
+    - *Obs: Utiilize os filtros da barra lateral para auxílio no estudo e visualização.*
     ---
-    - Visão Geral:
-            - Métricas gerais e de comportamentos 
-            - Insights de geolocalização
-    - Visão País:
-        - Geral: Métricas gerais e de acompanhamento dos países cadastrados
-        - Estratégica: Métricas estratégicas de média de preços e avaliações de países
-        - Descritiva: Uma base de dados resumida com métricas dos países
-    - Visão Cidade:
-        - Geral: Métricas gerais e de acompanhamento das cidades cadastradas
-        - Descritiva: Uma base de dados resumida com métricas das cidades
-    - Visão Restaurante:
-        - Geral: Métricas gerais e de acompanhamento dos restaurantes cadastrados
-        - Estratégica: Métricas estratégicas de média de preços e avaliações de restaurantes
-        - Tipos de Culinária: Gráficos e métricas dos tipos únicos de culinária
-        - Descritiva: Uma base de dados resumida com métricas restaurantes
-    - Visão Data Base:
-        - A base de dados uitlizada após a limpeza e tratamento dos dados
-    ### Ask for Help
-    - Fernando Silva
-        - e-mail: fernando.h.o.s@hotmail.com
-        - LinekdIn: www.linkedin.com/in/fernando-h-silva-
+''')
+
+col1,col2,col3 = st.columns(3,gap='Small')
+
+with col1:
+    if st.button("Visão Geral"):
+        st.switch_page("pages/1_Visao_Geral.py")
+
+    if st.button("Visão País"):
+        st.switch_page("pages/2_Visao_País.py")
+
+with col2:
+    if st.button("Visão Cidade"):
+        st.switch_page("pages/3_Visao_Cidade.py")
+
+    if st.button("Visão Restaurantes"):
+        st.switch_page("pages/4_Visao_Restaurante.py")
+with col3:
+    if st.button("Data Base"):
+        st.switch_page("pages/5_Data_Base.py")
+
+st.markdown('''  
+            ---
+            #### Para mais informações:''')
+
+if st.button('Clique'):
+    st.switch_page('pages/6_Ajuda.py')
+
+st.markdown('''
+            #### Ou contate
+            - Fernando Silva
+                - e-mail: fernando.h.o.s@hotmail.com
+                - LinekdIn: www.linkedin.com/in/fernando-h-silva-
 '''
 )   
 
@@ -46,9 +60,11 @@ st.markdown(
 
 
 
+with st.sidebar:
+    st.image( image, width=50 )
 
-st.sidebar.image( image, width=50 )
-
-st.sidebar.markdown( '# Fome zero ' ) #markdown altera o nível e tamanho da fonte que está no parênteses dependendo de quantos '#' existirem
-st.sidebar.markdown( '## All your favorite restaurants here' )
-st.sidebar.markdown( '''---''' )
+    st.markdown( '# Fome zero ' ) #markdown altera o nível e tamanho da fonte que está no parênteses dependendo de quantos '#' existirem
+    st.markdown( '## All your favorite restaurants here' )
+    st.markdown('''---''')
+    if st.button('Ajuda'):
+        st.switch_page('pages/6_Ajuda.py')
